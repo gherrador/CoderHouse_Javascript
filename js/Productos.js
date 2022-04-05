bf3d357a1ce6f12ad783538aa5e08a2303135f31
gherrador <82460834+gherrador@users.noreply.github.com>
2022-04-05 20:46:10 -0300

    Update Productos.js
    Se modifica funcionamiento de filtro.
    Ahora funciona de la siguiente manera:
    
    - Si se aplica un filtro por categoría de producto y luego un filtro por rango de precio, se mostraran los productos de esa categoría en ese rango de precio. En caso de eliminar el filtro precio, se mostraran los elementos por categoría filtrados anteriormente, en caso de eliminarse el filtro por la categoría, se mostraran los elementos por rango de precio.
    
    - Si se aplica un filtro por rango de precio de producto y luego un filtro por categoría, se mostraran los productos en ese rango de precio y esa categoría. En caso de eliminar el filtro precio, se mostraran los elementos por categoría filtrados anteriormente, en caso de eliminarse el filtro por la categoría, se mostraran los elementos por rango de precio.

--- js/Productos.js
+++ js/Productos.js
@@ -1,73 +1,119 @@
 import { renderCard } from './main.js'
+let productos = []
+let productosPrecio = []
 
-export const loadProducts = async()=> {
+export const loadProducts = async () => {
     try {
-      var response = await fetch('./BD/productos.json')
-      return response.json();
+        var response = await fetch('./BD/productos.json')
+        return response.json();
     } catch (err) {
-      console.error('la base de datos no ha sido cargada correctamente',err);
+        console.error('la base de datos no ha sido cargada correctamente', err);
     }
-  }
+}
 
 //Al apretar sobre el boton filtrado, cambiara la clase del boton y su css (rojo para eliminar filtro y verde para aplicar filtro)
 $('#filtrar_Productos').on('click', e => {
     try {
-        $('#filtrar_Productos').attr('Class')==='btn btn-success' ? 
-        $('#filtrar_Productos').text('Eliminar Filtro').removeClass('btn-success').addClass('btn-danger').css({'background': 'linear-gradient(90deg, rgba(249,71,71,1) 0%, rgba(177,0,0,1) 100%, rgba(255,0,0,1) 100%','box-shadow': '0 0.7em 1.5em -0.5em rgba(93, 0, 0, 0.8)'})&& filtroProductos() :
-        $('#filtrar_Productos').text('Aplicar Filtro').removeClass('btn-danger').addClass('btn-success').css({'background': 'linear-gradient(0deg, rgba(20, 167, 62, 1) 0%, rgba(102, 247, 113, 1) 100%', 'box-shadow': '0 0.7em 1.5em -0.5em #14a73e98'})&& eliminarFiltro()
+        $('#filtrar_Productos').attr('Class') === 'verde' ?
+            $('#filtrar_Productos').text('Eliminar Filtro').toggleClass('rojo') && filtroProductos() :
+            $('#filtrar_Productos').text('Aplicar Filtro').toggleClass('rojo') && eliminarFiltro()
         e.preventDefault()
     } catch (err) {
-        console.error("el boton de filtro no funciona correctamente",err)
+        console.error("el boton de filtro no funciona correctamente", err)
     }
 })
 
 //Al apretar sobre el boton filtrado, cambiara la clase del boton y su css (rojo para eliminar filtro y verde para aplicar filtro)
 $('#filtrar_Precio').on('click', e => {
     try {
-        $('#filtrar_Precio').attr('Class')==='btn btn-success' ? 
-        $('#filtrar_Precio').text('Eliminar Filtro').removeClass('btn-success').addClass('btn-danger').css({'background': 'linear-gradient(90deg, rgba(249,71,71,1) 0%, rgba(177,0,0,1) 100%, rgba(255,0,0,1) 100%','box-shadow': '0 0.7em 1.5em -0.5em rgba(93, 0, 0, 0.8)'}) && filtroPrecio() :
-        $('#filtrar_Precio').text('Aplicar Filtro').removeClass('btn-danger').addClass('btn-success').css({'background': 'linear-gradient(0deg, rgba(20, 167, 62, 1) 0%, rgba(102, 247, 113, 1) 100%', 'box-shadow': '0 0.7em 1.5em -0.5em #14a73e98'}) && eliminarFiltro()
+        $('#filtrar_Precio').attr('Class') === 'verde' ?
+            $('#filtrar_Precio').text('Eliminar Filtro').toggleClass('rojo') && filtroPrecio() :
+            $('#filtrar_Precio').text('Aplicar Filtro').toggleClass('rojo') && eliminarFiltroPrecio()
         e.preventDefault()
     } catch (err) {
-        console.error("El boton de filtro no funciona correctamente",err)
+        console.error("El boton de filtro no funciona correctamente", err)
     }
 })
 
-//Funcion para filtrar productos por categoria
+//Funcion para filtrar productos por categoria. 
+//Si existe ya el filtro por precio aplicado, filtra sobre los productos mostrados por rango de precio
 const filtroProductos = async () => {
     try {
-        const datos = await loadProducts()
         let opcion = ''
         $('.Comidas').is(':checked') && (opcion = $('.Comidas').val()) && ($('#titulo_productos').html(`<h2>Comidas:</h>`))
         $('.utensillosCocina').is(':checked') && (opcion = $('.utensillosCocina').val()) && ($('#titulo_productos').html(`<h2>Utensillos de Cocina:</h>`))
         $('.bebidas').is(':checked') && (opcion = $('.bebidas').val()) && ($('#titulo_productos').html(`<h2>Bebidas:</h>`))
         $('.golosinas').is(':checked') && (opcion = $('.golosinas').val()) && ($('#titulo_productos').html(`<h2>Dulces/Golosinas:</h>`))
-        const data = datos.filter((el) => el.Categoria === opcion);
-        renderCard(data)
+        if (productosPrecio.length > 0) {
+            const productos = productosPrecio.filter((el) => el.Categoria === opcion);
+            renderCard(productos)
+        } else {
+            const datos = await loadProducts()
+            const data = datos.filter((el) => el.Categoria === opcion);
+            if (opcion != '') {
+                productos.length = 0
+                data.forEach((value, index) => {
+                    productos.push(value)
+                })
+            }
+            renderCard(data)
+        }
     } catch (err) {
-        console.error("El filtro no funciona correctamente",err)
+        console.error("El filtro no funciona correctamente", err)
     }
 }
 
 //Funcion para filtrar productos por precio
+//Si existe ya un filtro aplicado por categoria de producto, se filtra sobre esa categoria
 const filtroPrecio = async () => {
     try {
         const datos = await loadProducts()
-        const data = datos.filter(el => (el.Precio >= $('#slider_valueMin').text() && el.Precio <= $('#slider_valueMax').text()));
-        $('#titulo_productos').html(`<h2>Productos entre $${$('#slider_valueMin').text()} y $${$('#slider_valueMax').text()}</h>`)
-        renderCard(data)
+        const precioMenor = $('#slider_valueMin').text()
+        const precioMayor = $('#slider_valueMax').text()
+        if (productos.length > 0) {
+            const data = productos.filter(el => (el.Precio >= precioMenor && el.Precio <= precioMayor));
+            renderCard(data)
+        } else {
+            const data = datos.filter(el => (el.Precio >= precioMenor && el.Precio <= precioMayor));
+            $('#titulo_productos').html(`<h2>Productos entre $${$('#slider_valueMin').text()} y $${$('#slider_valueMax').text()}</h>`)
+            renderCard(data)
+            data.forEach((value, index) => {
+                productosPrecio.push(value)
+            })
+        }
     } catch (err) {
-        console.error("El filtro no funciona correctamente",err)
+        console.error("El filtro no funciona correctamente", err)
     }
 }
 
-//Al eliminar el filtro, devuelve todos los productos a la pantalla principal
-const eliminarFiltro = async() => {
-    try{
+//Elimina el filtro precio. Si existe una categoria seleccionada, se mostraran los productos de esa categoria
+const eliminarFiltroPrecio = async () => {
+    try {
+        if (productos.length > 0) {
+            renderCard(productos)
+        } else {
+            $('#titulo_productos').html(`<h2>Productos Destacados:</h>`)
+            const data = await loadProducts()
+            renderCard(data)
+            productosPrecio.length = 0
+        }
+    } catch (err) {
+        console.error("Fue imposible eliminar el filtro", err)
+    }
+}
+
+//Elimina el filtro categoria, si existen productos filtrados por rango de precio, mostraran esos productos
+const eliminarFiltro = async () => {
+    try {
+        if(productosPrecio.length >0){
+            renderCard(productosPrecio)
+        }else{
         $('#titulo_productos').html(`<h2>Productos Destacados:</h>`)
         const data = await loadProducts()
         renderCard(data)
-    }catch(err){
+        productos.length = 0
+        }
+    } catch (err) {
         console.error("Fue imposible eliminar el filtro", err)
     }
 }
